b2683cbef67e9ea1884d751dd210f02d4c7895b6
carsimoes <carlos.renato.simoes@gmail.com>
2024-04-07 14:55:28 -0300

    Note Repository creation

---
+++ SmartNotes.Data/NoteRepository.cs
@@ -0,0 +1,18 @@
+﻿using SmartNotes.Domain;
+
+namespace SmartNotes.Data
+{
+    public class NoteRepository
+    {
+        private List<Note> Notes = new List<Note>();
+
+        public IEnumerable<Note> GetAll
+        {
+            get { return Notes; }
+        }
+        public void Create(Note note)
+        {
+            Notes.Add(note);
+        }
+    }
+}
---
+++ SmartNotes/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartNotes.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
