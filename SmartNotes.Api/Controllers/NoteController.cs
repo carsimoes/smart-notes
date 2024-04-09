2fbb887ad5751dc9c20f78ba7dc1090518a5bda6
carsimoes <carlos.renato.simoes@gmail.com>
2024-04-09 19:41:15 -0300

    Unit tests creation, first steps

---
+++ SmartNotes.Api/Controllers/NoteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartNotes.Domain.Models;
+
+namespace SmartNotes.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NoteController
+    {
+        [HttpGet(Name = "GetNotes")]
+        public IEnumerable<Note> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Note
+            {
+                Id = 1,
+                Content = "Test",
+                Title = "Title"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ SmartNotes.Tests/NoteControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using SmartNotes.Api.Controllers;
+
+namespace SmartNotes.Tests
+{
+    public class NoteControllerTests
+    {
+        //private readonly Mock<INoteRepository> _mockRepo;
+        private readonly NoteController _controller;
+        public NoteControllerTests()
+        {
+            //_mockRepo = new Mock<INoteRepository>();
+            _controller = new NoteController();//_mockRepo.Object);
+        }
+    }
+}
